42fdad4a1f3a35b25da2538c2bbf673bd9777713
Sergiu Nagailic <nikro.md@gmail.com>
2023-05-21 23:45:06 +0200

    Use CEC commands.

--- node_helper.js
+++ node_helper.js
@@ -4,8 +4,8 @@
  * By Sergiu Nagailic
  * MIT Licensed.
  */
-
-var NodeHelper = require("node_helper");
+const exec = require('child_process').exec;
+let NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
 
@@ -18,15 +18,96 @@ module.exports = NodeHelper.create({
 	 * argument payload mixed - The payload of the notification.
 	 */
 	socketNotificationReceived: function(notification, payload) {
-		if (notification === "MMM-WhisperGPT-Extra-NOTIFICATION_TEST") {
-			console.log("Working notification system. Notification:", notification, "payload: ", payload);
-			// Send notification
-			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
+		if (notification === "COMMAND") {
+			this.processCommand(payload);
+		}
+		else if (notification === 'KEYWORD_DETECTED') {
+			this.focusOn();
 		}
 	},
 
-	// Example function send notification test
-	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("MMM-WhisperGPT-Extra-NOTIFICATION_TEST", payload);
-	}
+  processCommand: function(command) {
+    // Convert the command to lowercase to handle case variations
+    command = command.toLowerCase();
+
+    // Remove any extra spaces
+    command = command.replace(/\s+/g, ' ').trim();
+
+    // Define regular expression patterns for each command
+    let turnOffPattern = /(turn|shut).*off/;
+    let shutDownPattern = /shut.*down/;
+    let turnOnPattern = /(turn|power).*on/;
+    let chromeCastPattern = /(chromecast)/;
+
+    // Check if command matches any pattern
+    if (turnOffPattern.test(command) || shutDownPattern.test(command)) {
+      // Command is to turn off the TV
+      console.log("Turning off the TV");
+      this.turnOff();
+      return;
+    }
+
+    if (turnOnPattern.test(command)) {
+      // Command is to turn on the TV
+      console.log("Turning on the TV");
+      this.turnOn();
+      return;
+    }
+
+    if (chromeCastPattern.test(command)) {
+      // Switch to Chromecast.
+      console.log("Switch to Chromecast");
+      this.focusOff();
+		}
+
+    // If none of the patterns matched, the command is not recognized
+    console.log("Command not recognized");
+  },
+
+
+  turnOn: function() {
+    exec('cec-ctl -d /dev/cec0 --to 0 --image-view-on', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  },
+
+  turnOff: function() {
+    exec('cec-ctl -d /dev/cec0 --to 0 --standby', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  },
+
+  focusOn: function() {
+    exec('cec-ctl -d /dev/cec0 --to 0 --active-source phys-addr=2.0.0.0', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  },
+
+  focusOff: function() {
+    exec('cec-ctl -d /dev/cec0 --to 0 --active-source phys-addr=1.0.0.0', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  }
+
+
 });
